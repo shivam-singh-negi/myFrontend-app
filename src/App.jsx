4e20b6299ae64ca098b9d0389f752c794888cc6c
shivam-singh-negi <shivamsingh.comp1999@gmail.com>
2023-10-06 19:34:23 +0530

    small

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 export default function App() {
   return (
-    <h1 className="text-3lg font-bold text-red-700 underline">
-      Hello world!
-    </h1>
+    <><h1 className="text-3lg font-bold text-red-700 underline">
+      whats up wotj ojsdjfla
+    </h1><h1 className="text-3lg font-bold text-red-700 underline">
+        whats up wotj ojsdjfla
+      </h1></>
   )
 }
